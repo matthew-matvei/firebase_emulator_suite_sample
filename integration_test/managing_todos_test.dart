a0b9d648099b67d82d0df3067c658a84cd3316b3
matthew-matvei <matthew.d.james87@gmail.com>
2023-10-28 10:54:40 +1000

    Support adding todo list items

--- integration_test/managing_todos_test.dart
+++ integration_test/managing_todos_test.dart
@@ -1,13 +1,39 @@
-import 'package:flutter/foundation.dart';
+import 'package:firebase_emulator_suite_sample/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:firebase_emulator_suite_sample/main.dart' as app;
+import 'package:firebase_emulator_suite_sample/main.dart' as app hide AppKeys;
 
 void main() {
   testWidgets("The user sees a list of todo items", (tester) async {
-    app.main();
-    await tester.pumpAndSettle();
+    await tester.runApp();
 
     expect(find.text("Todos"), findsOneWidget);
-    expect(find.byKey(const ValueKey<String>("TodoList")), findsOneWidget);
+    expect(find.byKey(AppKeys.todoList), findsOneWidget);
   });
+
+  testWidgets("The user can create new todo list items", (tester) async {
+    await tester.runApp();
+
+    await tester.createTodoListItem("My todo item");
+    await tester.createTodoListItem("Another todo item");
+    await tester.createTodoListItem("I'm getting bored now");
+
+    expect(find.text("My todo item"), findsOneWidget);
+    expect(find.text("Another todo item"), findsOneWidget);
+    expect(find.text("I'm getting bored now"), findsOneWidget);
+  });
+}
+
+extension _TestRunner on WidgetTester {
+  Future<void> runApp() async {
+    app.main();
+    await pumpAndSettle();
+  }
+
+  Future<void> createTodoListItem(String name) async {
+    await tap(find.byKey(AppKeys.createNewTodo));
+    await pumpAndSettle();
+    await enterText(find.byKey(AppKeys.newTodoText), name);
+    await testTextInput.receiveAction(TextInputAction.done);
+    await pumpAndSettle();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ void main() {
   runApp(const _TodoListApp());
 }
 
-class _TodoListApp extends StatelessWidget {
+class _TodoListApp extends StatefulWidget {
   const _TodoListApp();
+
+  @override
+  State<_TodoListApp> createState() => _TodoListAppState();
+}
+
+class _TodoListAppState extends State<_TodoListApp> {
+  var _creatingNewTodo = false;
+  final _todoListItems = <String>[];
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +25,39 @@ class _TodoListApp extends StatelessWidget {
         body: Center(
           child: ListView(
             key: const ValueKey<String>("TodoList"),
+            children: [
+              if (_creatingNewTodo)
+                TextField(
+                  key: AppKeys.newTodoText,
+                  autofocus: true,
+                  onSubmitted: (newTodoText) {
+                    setState(() {
+                      _todoListItems.add(newTodoText);
+                      _creatingNewTodo = false;
+                    });
+                  },
+                ),
+              if (!_creatingNewTodo)
+                ElevatedButton.icon(
+                    key: AppKeys.createNewTodo,
+                    onPressed: () {
+                      setState(() {
+                        _creatingNewTodo = true;
+                      });
+                    },
+                    icon: const Icon(Icons.add),
+                    label: const Text("Create new todo item")),
+              ..._todoListItems.map((todoListItem) => Text(todoListItem))
+            ],
           ),
         ),
       ),
     );
   }
+}
+
+class AppKeys {
+  static Key todoList = const ValueKey<String>("TodoList");
+  static Key createNewTodo = const ValueKey<String>("CreateNewTodo");
+  static Key newTodoText = const ValueKey<String>("NewTodoText");
 }
