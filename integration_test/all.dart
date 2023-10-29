8b54dec90946687c8d7b637322cd3ed840d0dd42
matthew-matvei <matthew.d.james87@gmail.com>
2023-10-29 21:22:12 +1000

    Use SDK to seed data

--- integration_test/all.dart
+++ integration_test/all.dart
@@ -1,9 +1,6 @@
-import 'dart:convert';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:http/http.dart' as http;
 
 import 'login_test.dart' as login;
 import 'managing_todos_test.dart' as managing_todos;
@@ -19,37 +16,16 @@ Future<void> main() async {
 
   FirebaseAuth.instance.useAuthEmulator("localhost", 9099);
 
-  final response = await http.post(
-      Uri.http("10.0.2.2:9099",
-          "/identitytoolkit.googleapis.com/v1/accounts:signUp", {"key": "any"}),
-      body: jsonEncode({
-        "email": "admin@fake.com",
-        "password": "Guest1!",
-        "returnSecureToken": "true"
-      }),
-      headers: {"Content-Type": "application/json"},
-      encoding: utf8);
-
-  if (response.statusCode != 200 && response.statusCode != 400) {
-    throw StateError(
-      "Received unexpected status code when signing up test user: ${response.statusCode} ${response.body}",
+  try {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: "admin@fake.com",
+      password: "Guest1!",
     );
-  }
-
-  if (response.statusCode == 400 && !response.body.contains("EMAIL_EXISTS")) {
-    throw StateError(
-      "Received unexpected error when signing up test user: '${response.body}'",
-    );
-  }
-
-  /*try {
-    await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(email: "admin", password: "Guest1!");
   } on FirebaseAuthException catch (ex) {
     if (ex.code != "email-already-in-use") {
       rethrow;
     }
-  }*/
+  }
 
   login.main();
   managing_todos.main();
