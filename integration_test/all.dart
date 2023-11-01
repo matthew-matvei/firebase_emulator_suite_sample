9fc558e1f4fb8095543305c62b0b9fcbe1e31626
matthew-matvei <matthew.d.james87@gmail.com>
2023-11-02 00:36:39 +1000

    Add new integration tests to all.dart

--- integration_test/all.dart
+++ integration_test/all.dart
@@ -1,7 +1,10 @@
 import 'login_test.dart' as login;
-import 'managing_todos_test.dart' as managingTodos;
+import 'managing_todos_test.dart' as managing_todos;
+import 'managing_todos_for_organisation_test.dart'
+    as managing_todos_for_organisation;
 
 void main() {
   login.main();
-  managingTodos.main();
+  managing_todos.main();
+  managing_todos_for_organisation.main();
 }
