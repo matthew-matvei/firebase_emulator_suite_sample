9a73909871f4f1d88c39b19eac121c924b2df6c6
matthew-matvei <matthew.d.james87@gmail.com>
2023-11-22 21:07:42 +1000

    Update Firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,20 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+exports.logCreations = functions.firestore.document("todos/{todoId}").onCreate((snapshot) => {
+  logger.info("Todo created with ID", snapshot.id)
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+  return snapshot;
+})
+
+exports.logUpdates = functions.firestore.document("todos/{todoId}").onUpdate((snapshot) => {
+  logger.info("Todo updated with ID", snapshot.after.id)
+
+  return snapshot;
+})
+
+exports.logDeletions = functions.firestore.document("todos/{todoId}").onDelete((snapshot) => {
+  logger.warn("Todo deleted with ID", snapshot.id)
+
+  return snapshot;
+})
